43b560fcd232f1e44ac102e89f76b8b870fbdd69
Will <will.lambert96@gmail.com>
2020-07-25 17:09:51 -0400

    Allow the host duhhh

--- shouldiwearamask/settings.py
+++ shouldiwearamask/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['localhost', '.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'shouldiwearamask-production.herokuapp.com']
 
 
 # Application definition
