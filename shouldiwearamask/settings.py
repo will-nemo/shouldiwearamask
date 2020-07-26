09a756301597bb6e994c57673f447573540edb76
Will <will.lambert96@gmail.com>
2020-07-26 00:26:21 -0400

    added site to allowed hosts

--- shouldiwearamask/settings.py
+++ shouldiwearamask/settings.py
@@ -26,7 +26,11 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
-ALLOWED_HOSTS = ['localhost', 'shouldiwearamask-production.herokuapp.com']
+ALLOWED_HOSTS = [
+    'localhost', 
+    'shouldiwearamask-production.herokuapp.com',
+    'shouldiwearamask.site'
+    ]
 
 
 # Application definition
